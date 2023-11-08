1dc51ca4f1d6fad668b8f48caf839ca50fd9bce0
Riccardo <riccardorocco2001@gmail.com>
2023-11-08 14:44:20 +0100

    correzione e bonus

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-const numCellePerRiga = 10;
-const numCelleTotali = 100;
-const grid = document.getElementById("grid");
-const creaGrigliaBtn = document.getElementById("creaGrigliaBtn");
+document.getElementById('generate-grid').addEventListener('click', generateGrid);
 
-creaGrigliaBtn.addEventListener("click", () => {
-    grid.innerHTML = "";
+function generateGrid() {
+  var gridContainer = document.getElementById('grid-container');
+  gridContainer.innerHTML = '';
 
-    for (let i = 1; i <= numCelleTotali; i++) {
-        const cella = document.createElement("div");
-        cella.classList.add("square");
-        cella.textContent = i;
-        grid.appendChild(cella);
+  var difficulty = document.getElementById('difficulty').value;
+  var gridSize = 0;
+  var gridRows = 0;
+  var gridColumns = 0;
 
-        cella.addEventListener("click", function() {
-            console.log("Cella cliccata", i);
-            cella.classList.toggle("highlight");
-        });
-    }
-});
+  if (difficulty === '1') {
+    gridSize = 100;
+    gridRows = 10;
+    gridColumns = 10;
+  } else if (difficulty === '2') {
+    gridSize = 81;
+    gridRows = 9;
+    gridColumns = 9;
+  } else if (difficulty === '3') {
+    gridSize = 64;
+    gridRows = 8;
+    gridColumns = 8;
+  }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`;
+
+  for (var i = 1; i <= gridSize; i++) {
+    var cell = document.createElement('div');
+    cell.classList.add('grid-cell');
+    cell.textContent = i;
+    gridContainer.appendChild(cell);
+  }
+
+  var gridCells = document.getElementsByClassName('grid-cell');
+
+  for (var i = 0; i < gridCells.length; i++) {
+    gridCells[i].addEventListener('click', handleClick);
+    gridCells[i].addEventListener('contextmenu', handleRightClick);
+  }
+}
+
+function handleClick(event) {
+  var cell = event.target;
+  cell.classList.toggle('clicked');
+  console.log('Cella cliccata:', cell.textContent);
+}
+
+function handleRightClick(event) {
+  event.preventDefault();
+  var cell = event.target;
+  cell.classList.remove('clicked');
+}
